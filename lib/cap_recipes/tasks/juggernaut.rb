7a62f5dd4db47617e92b0e81f7de552ebd374a86
achiu <achiu@Teumessian.local>
2009-10-27 00:30:40 -0700

    added description for packages

--- lib/cap_recipes/tasks/juggernaut.rb
+++ lib/cap_recipes/tasks/juggernaut.rb
@@ -1,1 +1,3 @@
+#Juggernaut install and management
+
 Dir[File.join(File.dirname(__FILE__), 'juggernaut/*.rb')].sort.each { |lib| require lib }
--- lib/cap_recipes/tasks/memcache.rb
+++ lib/cap_recipes/tasks/memcache.rb
@@ -1,1 +1,3 @@
+# Manages memcache
+
 Dir[File.join(File.dirname(__FILE__), 'memcache/*.rb')].sort.each { |lib| require lib }
