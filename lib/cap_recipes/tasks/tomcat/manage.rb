014384343a01050baaafbe5f09434f27ac1df6d3
crazycode <crazycode@gmail.com>
2010-09-05 17:11:49 +0800

    update tomcat recipes.

--- lib/cap_recipes/tasks/tomcat/manage.rb
+++ lib/cap_recipes/tasks/tomcat/manage.rb
@@ -1,32 +1,34 @@
-#
-# simple interactions with the tomcat server
-#
-namespace :tomcat do
+Capistrano::Configuration.instance(true).load do
+  #
+  # simple interactions with the tomcat server
+  #
+  namespace :tomcat do
 
-  desc "start tomcat"
-  task :start do
-    sudo "#{tomcat_ctrl} start"
+    desc "start tomcat"
+    task :start do
+      sudo "#{tomcat_ctrl} start"
+    end
+
+    desc "stop tomcat"
+    task :stop do
+      sudo "#{tomcat_ctrl} stop"
+    end
+
+    desc "stop and start tomcat"
+    task :restart do
+      tomcat.stop
+      tomcat.start
+    end
+
+    desc "list tomcat process"
+    task :ps do
+      run "ps aux | grep tomcat | grep -v grep"
+    end
+
+    desc "tail :tomcat_home/logs/*.log and logs/catalina.out"
+    task :tail do
+      stream "tail -f #{tomcat_home}/logs/*.log #{tomcat_home}/logs/catalina.out"
+    end
+
   end
-
-  desc "stop tomcat"
-  task :stop do
-    sudo "#{tomcat_ctrl} stop"
-  end
-
-  desc "stop and start tomcat"
-  task :restart do
-    tomcat.stop
-    tomcat.start
-  end
-
-  desc "list tomcat process"
-  task :ps do
-    run "ps aux | grep tomcat | grep -v grep"
-  end
-
-  desc "tail :tomcat_home/logs/*.log and logs/catalina.out"
-  task :tail do
-    stream "tail -f #{tomcat_home}/logs/*.log #{tomcat_home}/logs/catalina.out"
-  end
-
 end
--- lib/cap_recipes/tasks/tomcat/war.rb
+++ lib/cap_recipes/tasks/tomcat/war.rb
@@ -1,87 +1,89 @@
-# DEPLOYMENT SCHEME
-set :scm, :none
-set :deploy_via, :copy
-set :use_sudo, true
-default_run_options[:pty] = true
+Capistrano::Configuration.instance(true).load do
+  # DEPLOYMENT SCHEME
+  set :scm, :none
+  set :deploy_via, :copy
+  set :use_sudo, true
+  default_run_options[:pty] = true
 
-set :keep_releases, 5
+  set :keep_releases, 5
 
-set :repository do
-  fetch(:deploy_from)
-end
-
-set :deploy_from do
-  dir = "/tmp/prep_#{release_name}"
-  system("mkdir -p #{dir}")
-  dir
-end
-
-# this is capistrano's default location.
-# depending on the permissions of the server
-# you may need to create it and chown it over
-# to :user (e.g. chown -R robotuser:robotuser /u)
-set :deploy_to do
-  "#{deploy_dir}/#{application}"
-end
-
-#
-# link the current/whatever.war into our webapps/whatever.war
-#
-#after 'deploy:setup' do
-#  cmd = "ln -s #{deploy_to}/current/`basename #{war}` #{tomcat_home}/webapps/`basename #{war}`"
-#  puts cmd
-#  sudo cmd
-#end
-
-# collect up our war into the deploy_from folder
-# notice that all we're doing is a copy here,
-# so it is pretty easy to swap this out for
-# a wget command, which makes sense if you're
-# using a continuous integration server like
-# bamboo. (more on this later).
-before 'deploy:update_code' do
-  unless(war.nil?)
-    puts "get war"
-    system("mkdir #{deploy_from}/webapp")
-    system("cp #{war} #{deploy_from}/webapp")
-    system("cd #{deploy_from}/webapp && jar xf `basename #{war}`")
-    system("rm -Rf #{deploy_from}/webapp/META-INF")
-    system("rm #{deploy_from}/webapp/`basename #{war}`")
-    puts system("ls -l #{deploy_from}")
-  end
-end
-
-#
-# Disable all the default tasks that
-# either don't apply, or I haven't made work.
-#
-namespace :deploy do
-  # restart tomcat
-  task :restart do
-    tomcat.restart
+  set :repository do
+    fetch(:deploy_from)
   end
 
-  [ :upload, :cold, :start, :stop, :migrate, :migrations, :finalize_update ].each do |default_task|
-    desc "[internal] disabled"
-    task default_task do
-      # disabled
+  set :deploy_from do
+    dir = "/tmp/prep_#{release_name}"
+    system("mkdir -p #{dir}")
+    dir
+  end
+
+  # this is capistrano's default location.
+  # depending on the permissions of the server
+  # you may need to create it and chown it over
+  # to :user (e.g. chown -R robotuser:robotuser /u)
+  set :deploy_to do
+    "#{deploy_dir}/#{application}"
+  end
+
+  #
+  # link the current/whatever.war into our webapps/whatever.war
+  #
+  #after 'deploy:setup' do
+  #  cmd = "ln -s #{deploy_to}/current/`basename #{war}` #{tomcat_home}/webapps/`basename #{war}`"
+  #  puts cmd
+  #  sudo cmd
+  #end
+
+  # collect up our war into the deploy_from folder
+  # notice that all we're doing is a copy here,
+  # so it is pretty easy to swap this out for
+  # a wget command, which makes sense if you're
+  # using a continuous integration server like
+  # bamboo. (more on this later).
+  before 'deploy:update_code' do
+    unless(war.nil?)
+      puts "get war"
+      system("mkdir #{deploy_from}/webapp")
+      system("cp #{war} #{deploy_from}/webapp")
+      system("cd #{deploy_from}/webapp && jar xf `basename #{war}`")
+      system("rm -Rf #{deploy_from}/webapp/META-INF")
+      system("rm #{deploy_from}/webapp/`basename #{war}`")
+      puts system("ls -l #{deploy_from}")
     end
   end
 
-  namespace :web do
-    [ :disable, :enable ].each do |default_task|
+  #
+  # Disable all the default tasks that
+  # either don't apply, or I haven't made work.
+  #
+  namespace :deploy do
+    # restart tomcat
+    task :restart do
+      tomcat.restart
+    end
+
+    [ :upload, :cold, :start, :stop, :migrate, :migrations, :finalize_update ].each do |default_task|
       desc "[internal] disabled"
       task default_task do
         # disabled
       end
     end
-  end
 
-  namespace :pending do
-    [ :default, :diff ].each do |default_task|
-      desc "[internal] disabled"
-      task default_task do
-        # disabled
+    namespace :web do
+      [ :disable, :enable ].each do |default_task|
+        desc "[internal] disabled"
+        task default_task do
+          # disabled
+        end
+      end
+    end
+
+    namespace :pending do
+      [ :default, :diff ].each do |default_task|
+        desc "[internal] disabled"
+        task default_task do
+          # disabled
+        end
       end
     end
   end
