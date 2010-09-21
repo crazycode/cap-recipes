9245f1db32fe5ae6ceb069f3c09cd45c9e210734
crazycode <crazycode@gmail.com>
2010-09-21 19:29:27 +0800

    update maven code.

--- lib/cap_recipes/tasks/maven.rb
+++ lib/cap_recipes/tasks/maven.rb
@@ -1,4 +1,5 @@
 require 'fileutils'
+require 'capistrano/recipes/deploy/scm'
 require File.expand_path(File.dirname(__FILE__) + '/utilities')
 require File.expand_path(File.dirname(__FILE__) + '/tomcat')
 
@@ -6,47 +7,62 @@ Capistrano::Configuration.instance(true).load do
 
   set :source_path, "source"
 
-  set :source_dir do
+  set :scm, :subversion
 
+  set :source_dir do
+    "#{Dir.pwd}/#{source_path}"
   end
 
   namespace :mvn do
 
     desc "default mvn task. do svn update, package, and then deploy to tomcat"
     task :default do
-      puts Dir.pwd
+      update_source
+      build
     end
 
+    desc "update code from scm from maven build."
     task :update_source do
-      run "svn "
+      on_rollback { system "rm -rf #{source_dir}; true" }
+      if Dir.exists?(source_dir)
+        system source.sync(real_revision, source_dir)
+      else
+        system "mkdir #{source_dir}"
+        system source.checkout(real_revision, source_dir)
+      end
     end
 
-  end
-  namespace :deploy do
-    set :modules, {
-      :admin_module => {
-        :name => "message-admin",
-        :module_locate => "message-admin",
-        :war_name => "message-admin.war",
-        :webserver => "10.241.12.44"
-      },
-      :web_module => {
-        :name => "message-web",
-        :module_locate => "message-admin",
-        :war_name => "message-admin.war",
-        :webserver => "10.241.12.44"
-      }
-    }
+    desc "build use maven2"
+    task :build do
+      system "cd #{source_dir}; mvn -Dmaven.test.skip=true clean install"
+    end
 
-    task :default do
+    task :clean do
+      if Dir.exists?("#{source_dir}/.svn")
+        system "cd #{source_dir}; mvn clean"
+      end
+    end
+
+    desc "deploy all module"
+    task :deploy do
       if exists?(:modules)
+        #default
         modules.each do |key, module_setting|
-          set :name, module_setting[:name]
-          set :war, File.dirname(__FILE__) + "/svn/message/message-web/target/message-web.war"
-          set :war,
+          set :scm, :none
+          set :application, module_setting[:name]
+          set :module_dir, module_setting[:module_dir].nil? ? name : module_setting[:module_dir]
+          set :war, "#{source_dir}/#{module_dir}/target/#{module_setting[:war_name]}"
+          set :webserver, module_setting[:webserver]
+
+          set :scm, :none
+          set :deploy_via, :copy
+          set :use_sudo, true
+
+          deploy.default
         end
       end
     end
 
   end
+
 end
