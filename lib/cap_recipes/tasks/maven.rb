30ea80c829be2e7aa0c83dac141d2febf061f31c
crazycode <crazycode@gmail.com>
2010-10-08 19:41:25 +0800

    update maven recipes.

--- lib/cap_recipes/tasks/maven.rb
+++ lib/cap_recipes/tasks/maven.rb
@@ -13,76 +13,73 @@ Capistrano::Configuration.instance(true).load do
     "#{Dir.pwd}/#{source_path}"
   end
 
-  namespace :mvn do
+  desc "default mvn task. do svn update, package, and then deploy to tomcat"
+  task :default do
+    update_source
+    build
+  end
 
-    desc "default mvn task. do svn update, package, and then deploy to tomcat"
-    task :default do
-      update_source
-      build
+  desc "update code from scm from maven build."
+  task :update_source do
+    on_rollback { system "rm -rf #{source_dir}; true" }
+    if Dir.exists?(source_dir)
+      system source.sync(real_revision, source_dir)
+    else
+      system "mkdir #{source_dir}"
+      system source.checkout(real_revision, source_dir)
     end
+  end
 
-    desc "update code from scm from maven build."
-    task :update_source do
-      on_rollback { system "rm -rf #{source_dir}; true" }
-      if Dir.exists?(source_dir)
-        system source.sync(real_revision, source_dir)
-      else
-        system "mkdir #{source_dir}"
-        system source.checkout(real_revision, source_dir)
+  desc "build use maven2"
+  task :build do
+    system "cd #{source_dir}; mvn -Dmaven.test.skip=true clean install"
+  end
+
+  task :clean do
+    if Dir.exists?("#{source_dir}/.svn")
+      system "cd #{source_dir}; mvn clean"
+    end
+  end
+
+  desc "deploy all module"
+  task :mdeploy do
+    if exists?(:modules)
+      #default
+      modules.each do |key, module_setting|
+        set :scm, :none
+        set :application, module_setting[:name]
+        set :module_dir, module_setting[:module_dir].nil? ? name : module_setting[:module_dir]
+        set :war, "#{source_dir}/#{module_dir}/target/#{module_setting[:war_name]}"
+        set :webserver, module_setting[:webserver]
+
+        set :scm, :none
+        set :deploy_via, :copy
+        set :use_sudo, true
+
+        deploy.update_code :roles => webserver
       end
     end
+  end
+  desc "deploy all module"
+  task :msetup do
+    if exists?(:modules)
+      #default
+      modules.each do |key, module_setting|
+        puts "do #{key}...#{module_setting[:name]}"
+        set :scm, :none
+        set :application, module_setting[:name]
+        set :module_dir, module_setting[:module_dir].nil? ? name : module_setting[:module_dir]
+        set :war, "#{source_dir}/#{module_dir}/target/#{module_setting[:war_name]}"
+        set :webserver, module_setting[:webserver]
 
-    desc "build use maven2"
-    task :build do
-      system "cd #{source_dir}; mvn -Dmaven.test.skip=true clean install"
-    end
+        set :scm, :none
+        set :deploy_via, :copy
+        set :use_sudo, true
 
-    task :clean do
-      if Dir.exists?("#{source_dir}/.svn")
-        system "cd #{source_dir}; mvn clean"
+        deploy.setup :roles => webserver
       end
     end
-
-    desc "deploy all module"
-    task :mdeploy do
-      if exists?(:modules)
-        #default
-        modules.each do |key, module_setting|
-          set :scm, :none
-          set :application, module_setting[:name]
-          set :module_dir, module_setting[:module_dir].nil? ? name : module_setting[:module_dir]
-          set :war, "#{source_dir}/#{module_dir}/target/#{module_setting[:war_name]}"
-          set :webserver, module_setting[:webserver]
-
-          set :scm, :none
-          set :deploy_via, :copy
-          set :use_sudo, true
-
-          deploy.update_code :roles => webserver
-        end
-      end
-    end
-    desc "deploy all module"
-    task :msetup do
-      if exists?(:modules)
-        #default
-        modules.each do |key, module_setting|
-          puts "do #{key}...#{module_setting[:name]}"
-          set :scm, :none
-          set :application, module_setting[:name]
-          set :module_dir, module_setting[:module_dir].nil? ? name : module_setting[:module_dir]
-          set :war, "#{source_dir}/#{module_dir}/target/#{module_setting[:war_name]}"
-          set :webserver, module_setting[:webserver]
-
-          set :scm, :none
-          set :deploy_via, :copy
-          set :use_sudo, true
-
-          deploy.setup :roles => webserver
-        end
-      end
-    end
-
   end
 
 end
+
