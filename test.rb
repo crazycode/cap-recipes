2e1901c6dcf2ae2468cb133abec016999cee2e91
crazycode <crazycode@gmail.com>
2011-07-06 18:08:02 +0800

    update

--- test.rb
+++ test.rb
@@ -2,9 +2,6 @@ require "net/http"
 require "uri"
 require "json"
 
-require 'cap_recipes/tasks/gitdeploy'
-require File.expand_path(File.dirname(__FILE__) + '/http')
-
 class CmdbService
 
   def self.get_app_role(cse_base, unit_code, stage)
@@ -24,23 +21,26 @@ class CmdbService
     return result
   end
 
-end
 
+  def self.start_deploy(cse_base, unit_code, stage)
+    url = "#{cse_base}/deploy/get-server.do?deployUnitCode=#{unit_code}&stage=#{stage}"
+    resp = Net::HTTP.get_response(URI.parse(url))
+    data = resp.body
 
-Capistrano::Configuration.instance(true).load do |configuration|
-  set :use_sudo, true
+    # we convert the returned JSON data to native Ruby
+    # data structure - a hash
+    result = JSON.parse(data)
 
-  set :cse_base, "http://10.241.14.166:8080/cse"
-  set :deploy_unit_code, ""
-  set :deploy_stage, "development"
-
-  namespace :cmdb do
-
-    desc "start cmdb deploy"
-    task :start do
-      s = CmdbService.get_app_role("#{cse_base}", deploy_unit_code, deploy_stage)
-      puts "s=#{s}"
+    # if the hash has 'Error' as a key, we raise an error
+    if !result.has_key? 'success'
+      raise "web service error"
     end
 
+    return result
   end
+
+
 end
+
+s = CmdbService.get_app_role("http://10.241.14.166:8080/cse", "zf00032", "test")
+puts "s=#{s}"
